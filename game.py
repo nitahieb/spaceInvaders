dc55d9156c00b1e1401e6564ec0e6f38e51a662e
nitahieb <nitahieb@gmail.com>
2021-07-15 22:16:18 -0700

    added player bullet visuals

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
 WHITE = (255,255,255)
+BLACK = (0,0,0)
 
 
 FPS = 60
@@ -20,9 +21,14 @@ SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets","spaceship_red.png"))
 
 SPACESHIP= pygame.transform.rotate(pygame.transform.scale(SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),180)
 
-def draw_window(spaceship):
+def draw_window(spaceship,player_bullets):
     WIN.fill(WHITE)
     WIN.blit(SPACESHIP,(spaceship.x,spaceship.y))
+    for bullet in player_bullets:
+        pygame.draw.rect(WIN,BLACK,bullet)
+
+
+
     pygame.display.update()
 
 def spaceship_movement(keys_pressed,spaceship):
@@ -40,12 +46,7 @@ def spaceship_movement(keys_pressed,spaceship):
     elif VEL == 7:
         VEL = 5
 
-def handle_player_bullets(keys_pressed,spaceship):
-    player_bullets = []
-    if keys_pressed[pygame.K_SPACE]:
-        bullet = pygame.Rect(spaceship.x + spaceship.width,spaceship.y + spaceship.height/2, 10, 5)
-        player_bullets.append(bullet)
-    
+def handle_player_bullets(player_bullets,spaceship):
     for bullet in player_bullets:
         bullet.y -= BULLET_VEL
 
@@ -53,6 +54,7 @@ def handle_player_bullets(keys_pressed,spaceship):
 
 def main():
     spaceship = pygame.Rect(430,450,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
+    player_bullets = []
 
 
     clock = pygame.time.Clock()
@@ -62,12 +64,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bullet = pygame.Rect(spaceship.x + spaceship.width//2-2   ,spaceship.y + spaceship.height//2, 5, 10)
+                    player_bullets.append(bullet)
+
 
         keys_pressed = pygame.key.get_pressed()
         spaceship_movement(keys_pressed,spaceship)
         
-        handle_player_bullets(keys_pressed,spaceship)
-        draw_window(spaceship)
+        handle_player_bullets(player_bullets,spaceship)
+        draw_window(spaceship,player_bullets)
 
 
     pygame.quit()
