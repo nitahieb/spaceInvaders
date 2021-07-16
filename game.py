2022400fa8220572787a7ddde970f5314fdc7637
nitahieb <nitahieb@gmail.com>
2021-07-16 16:58:39 -0700

    Main game almost complete, added health bars, bullet and ship collisions

--- game.py
+++ game.py
@@ -50,26 +50,52 @@ class Laser:
 
     def move(self, vel):
         self.y += vel
-
+    
     def off_screen(self,height):
-        return self.y <= height and self.y >= 0
+        return not (self.y <= height and self.y+30 >= 0)
 
     def collision(self, obj):
         return collide(obj,self)
 
 class Ship:
+    COOLDOWN = 20
     def __init__(self,x,y,health=100):
         self.x = x
         self.y = y
         self.health = health
         self.ship_img = None 
+        self.laser_img = None
         self.lasers = []
         self.cool_down_counter = 0
     
     def draw(self):
-        pygame.Rect(430,450,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
+        pygame.Rect(430,450,self.x,self.y)
         WIN.blit(self.ship_img,(self.x,self.y))
+        for laser in self.lasers: 
+            laser.draw()
 
+    def move_lasers(self,vel,obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter +=1
+    
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x-22,self.y-self.get_height(),self.laser_img)   
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
+    
     def get_width(self):
         return self.ship_img.get_width()
 
@@ -81,10 +107,32 @@ class Player(Ship):
     def __init__(self,x,y,health=100):
         super().__init__(x,y,health)
         self.ship_img = SPACESHIP
+        self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
         self.vel = VEL
         self.boost = False
+
+    def move_lasers(self,vel,objs):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            else:
+                for obj in objs:
+                    if laser.collision(obj):
+                        objs.remove(obj)
+                        self.lasers.remove(laser)
+    
+    def draw(self):
+        super().draw()
+        self.healthbar()
+    
+    def healthbar(self):
+        pygame.draw.rect(WIN,(255,0,0),(self.x,self.y+self.ship_img.get_height()+10, self.ship_img.get_width(),10))
+        pygame.draw.rect(WIN,(0,255,0),(self.x,self.y+self.ship_img.get_height()+10, self.ship_img.get_width()*(self.health/self.max_health),10))
+
 
 
 class Enemy(Ship):
@@ -94,14 +142,23 @@ class Enemy(Ship):
         "blue":(BLUE_SPACE_SHIP,BLUE_LASER)
     }
 
-
     def __init__(self ,x ,y ,color ,health=100):
         super().__init__(x, y, health)
+        self.color = color
         self.ship_img, self.laser_img = self.COLOR_MAP[color]
         self.mask = pygame.mask.from_surface(self.ship_img)
 
     def move(self,vel):
         self.y+= vel
+    
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            if self.color == "blue":
+                laser = Laser(self.x-25,self.y,self.laser_img)
+            else:
+                laser = Laser(self.x-15,self.y,self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
 
 
@@ -115,16 +172,12 @@ def spaceship_movement(keys_pressed,spaceship):
     if keys_pressed[pygame.K_s] and spaceship.y+spaceship.vel+spaceship.get_height() < HEIGHT: #down
         spaceship.y += spaceship.vel
     if keys_pressed[pygame.K_LSHIFT]: #boost        
-        spaceship.vel = 10
+        spaceship.vel = 7
         spaceship.boost=True
-    elif spaceship.vel == 10:
+    elif spaceship.vel == 7:
         spaceship.vel = 5
         spaceship.boost = False
 
-
-def handle_player_bullets(player_bullets,spaceship):
-    for bullet in player_bullets:
-        bullet.y -= BULLET_VEL
 
 
 def collide(obj1, obj2):
@@ -134,11 +187,12 @@ def collide(obj1, obj2):
 
 def main():
     #spaceship = pygame.Rect(430,450,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
-    player_bullets = []
     level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans",30)
     lost_font = pygame.font.SysFont("comicsans",60)
+
+    laser_vel = 9
 
     enemies = []
     wave_length = 5
@@ -150,21 +204,16 @@ def main():
     lost_count = 0
     run = True
 
-    player = Player(430,450)
+    player = Player(430,440)
 
     def redraw_window():
         WIN.blit(BG,(0,0))
-        
-        
+
         if player.boost:
             WIN.blit(FIRE,(player.x+player.get_width()//2-7,player.y+player.get_height()))
-        for bullet in player_bullets:
-            pygame.draw.rect(WIN,BLACK,bullet)
-
 
         for enemy in enemies:
             enemy.draw()
-
         player.draw()
         lives_label = main_font.render(f"Lives: {lives}", 1, (255,0,255))
         level_label = main_font.render(f"Level: {level}",1,(255,0,255))
@@ -181,11 +230,19 @@ def main():
         clock.tick(FPS)
         redraw_window()
 
-        if lives <= 0 or player.health <=0:
+        if lives <= 0:
             lost = True
             lost_count += 1
 
+        if player.health <=0:
+            lives-= 1
+            del player
+            player = Player(430,440)
+            player.health = 100
+
         if lost:
+            del player
+            player = Player(1000,1000)
             if(lost_count> FPS*3 ):
                 run = False
             else:
@@ -201,27 +258,30 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    bullet = pygame.Rect(player.x + player.get_width()//2-2   ,player.y + player.get_height()//2, 5, 10)
-                    player_bullets.append(bullet)
-
 
         keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_SPACE]:
+            player.shoot()
+        
         spaceship_movement(keys_pressed,player)
 
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
-            if enemy.y+enemy.get_height() > HEIGHT:
+            enemy.move_lasers(laser_vel,player)
+
+            if random.randrange(0, 4*FPS) == 1:
+                enemy.shoot()
+
+            if collide(enemy,player):
+                player.health -= 100
+                enemies.remove(enemy)
+            elif enemy.y+enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
         
-        handle_player_bullets(player_bullets,player)
+        player.move_lasers(-laser_vel,enemies)
         
 
-
-    pygame.quit()
 
 
 if __name__ == "__main__":
